6b7693967cb62d1635654da252abc6dc7d3d6283
rick-sys <ricksanchezy2x@gmail.com>
2024-02-24 00:04:29 +0100

    d

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import hero from "/public/hero.png";
-import AutoplayVideo from "@/components/AutoplayVideo";
+import AutoplayVideo from "@/components/ui/autoplayvideo";
 import { Button } from "@/components/ui/button";
 import ExplainerSection from "@/components/ExplainerSection";
 import PricingSection from "@/components/PricingSection";
--- components/ui/autoplayvideo.tsx
+++ components/ui/autoplayvideo.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 import React, { useRef, useEffect } from 'react';
 
 const AutoplayVideo = ({ src }) => {
