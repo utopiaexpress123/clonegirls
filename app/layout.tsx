f63bde92553d42c22f3fc18ad42a7162240bedf7
rick-sys <ricksanchezy2x@gmail.com>
2024-02-23 21:57:45 +0100

    radix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import '@radix-ui/themes/styles.css';
 import { Suspense } from "react";
 
 export const metadata = {
@@ -13,16 +14,18 @@ export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <section>
-          <Suspense fallback={<div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />}>
-            <Navbar />
-          </Suspense>
-        </section>
-        <main className="flex flex-1 flex-col items-center py-16">
-          {children}
-        </main>
-        <Footer />
-        <Toaster />
+        <Theme>
+          <section>
+            <Suspense fallback={<div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />}>
+              <Navbar />
+            </Suspense>
+          </section>
+          <main className="flex flex-1 flex-col items-center py-16">
+            {children}
+          </main>
+          <Footer />
+          <Toaster />
+        </Theme>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-
 import hero from "/public/hero.png";
 
 import { Button } from "@/components/ui/button";
@@ -23,7 +22,6 @@ export default async function Index() {
   }
 
   return (
-
 
     <div className="flex flex-col items-center pt-1">
       <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
