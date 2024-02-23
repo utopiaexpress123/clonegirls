e0fecec18cfbaa96d17d6fd1e90a5ba86526d84c
rick-sys <ricksanchezy2x@gmail.com>
2024-02-24 00:10:19 +0100

    rrr

--- components/ui/autoplayvideo.tsx
+++ components/ui/autoplayvideo.tsx
@@ -1,26 +1,9 @@
 "use client"
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 const AutoplayVideo = ({ src }) => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-
-    const handleEnded = () => {
-      video.currentTime = 0;
-      video.play();
-    };
-
-    video.addEventListener('ended', handleEnded);
-
-    return () => {
-      video.removeEventListener('ended', handleEnded);
-    };
-  }, []);
-
   return (
-    <video ref={videoRef} autoPlay loop>
+    <video autoPlay loop>
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
