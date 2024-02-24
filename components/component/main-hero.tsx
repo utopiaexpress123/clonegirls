fe0b79474521d599c005df4968a8000aa0fe189f
rick-sys <ricksanchezy2x@gmail.com>
2024-02-25 00:28:04 +0100

    ee

--- components/component/main-hero.tsx
+++ components/component/main-hero.tsx
@@ -11,9 +11,8 @@ export function MainHero() {
         <div className="container px-4 md:px-6 flex flex-col items-start gap-2">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Meet your photogenic clone</h1>
           <p className="max-w-[700px] text-gray-500 dark:text-gray-400">
-          It's definitely you, just a better version.
-          Our AI-based service is able to "learn" your unique characteristics, 
-          create your digital counterpart, and take breathtaking pictures featuring you.
+          It's definitely you, just a better version. 
+          Create your digital counterpart, and take breathtaking pictures featuring you.
           </p>
           <Link
             className="inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
