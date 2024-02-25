fe829979a7007512ae8dc512ee6127924d57dc43
rick-sys <ricksanchezy2x@gmail.com>
2024-02-25 06:03:09 +0100

    Update main-hero.tsx

--- components/component/main-hero.tsx
+++ components/component/main-hero.tsx
@@ -17,7 +17,7 @@ export function MainHero() {
           
           <Link
             className="inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-            href="#"
+            href="./login"
             style={{
               borderRadius: "50px",
             }}
@@ -35,6 +35,7 @@ export function MainHero() {
             />
             Create Your Clone
           </Link>
+          <br></br>
           <p className="text-xs text-gray-500 dark:text-gray-400">
             Already a member?
             <Link className="underline" href="#">
