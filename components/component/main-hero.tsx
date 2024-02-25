35ce06669b8725a50ea7c299f9937a93bc4c601a
rick-sys <ricksanchezy2x@gmail.com>
2024-02-25 06:22:38 +0100

    Update main-hero.tsx

--- components/component/main-hero.tsx
+++ components/component/main-hero.tsx
@@ -14,7 +14,7 @@ export function MainHero() {
           It's definitely you, just a better version. 
           Create your digital counterpart, and take breathtaking pictures featuring you.
           </p>
-          
+          <p>
           <Link
             className="inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
             href="./login"
@@ -35,12 +35,12 @@ export function MainHero() {
             />
             Create Your Clone 
           </Link>
-          <br></br>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
+            <span className="text-xs text-gray-500 dark:text-gray-400">
             Already a member?
             <Link className="underline" href="#">
               Sign In
             </Link>
+            </span>
           </p>
         </div>
       </section>
