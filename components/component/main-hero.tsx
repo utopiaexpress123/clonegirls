4fd75cf6bf77b5987eeb959e62536ed6c3668c15
rick-sys <ricksanchezy2x@gmail.com>
2024-02-25 14:29:53 +0100

    d

--- components/component/main-hero.tsx
+++ components/component/main-hero.tsx
@@ -9,10 +9,10 @@ export function MainHero() {
     return (
       <section className="w-full">
         <div className="container px-4 md:px-6 flex flex-col items-start gap-2">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Bring to life your AI clone</h1>
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Bring to life your digital clone</h1>
           <p className="py-2 text-lg max-w-[700px] text-gray-500 dark:text-gray-400">
           Create your digital counterpart, and take breathtaking pictures featuring you.
-          It's definitely you, just a better version.
+          It's pure magic!
           </p>
           <div>
              <Link
@@ -33,7 +33,7 @@ export function MainHero() {
               }}
               width={34}
             />
-            for Girls&nbsp;&nbsp; 
+            Create your girl clone&nbsp;&nbsp; 
             </Link>
             <Link
               className="inline-flex w-100 items-center rounded-full shadow-lg hover:shadow-xl bg-blue-50 hover:bg-blue-100 dark:bg-blue-100 text-black-800 text-sm font-normal px-2 py-1 ml-2"
@@ -46,7 +46,7 @@ export function MainHero() {
               alt="Double helix"
               className="rounded-full mr-2"
               height={34}
-              src="/man.png"
+              src="/kep2.gif"
               style={{
                 aspectRatio: "34/34",
                 objectFit: "cover",
