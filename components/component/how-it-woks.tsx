5512efd9204bf9d166aed30ac376d64c854c39ce
rick-sys <ricksanchezy2x@gmail.com>
2024-02-25 04:09:17 +0100

    Update how-it-woks.tsx

--- components/component/how-it-woks.tsx
+++ components/component/how-it-woks.tsx
@@ -8,7 +8,7 @@ export function HowItWoks() {
   return (
     <>
       <Card className="flex w-full items-center p-4 shadow-lg border-0">
-        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-thin text-8xl">
+        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-bold text-8xl">
           01
         </div>
         <div className="grid gap-2 ml-6">
@@ -33,7 +33,7 @@ export function HowItWoks() {
         <div className="h-6 w-0.5 bg-gray-200 dark:bg-gray-800" />
       </div>
       <Card className="flex w-full items-center p-4 shadow-lg border-0">
-        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-thin text-8xl">
+        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-bold text-8xl">
           02
         </div>
         <div className="grid gap-2 ml-6">
@@ -46,7 +46,7 @@ export function HowItWoks() {
         <div className="h-6 w-0.5 bg-gray-200 dark:bg-gray-800" />
       </div>
       <Card className="flex w-full items-center p-4 shadow-lg border-0">
-        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-thin text-8xl">
+        <div className="flex items-center justify-center w-32 h-32 rounded-lg text-gray-100 dark:text-gray-100 font-bold text-8xl">
           03
         </div>
         <div className="grid gap-2 ml-6">
