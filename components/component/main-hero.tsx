6a840d8370ba970dc0703efd03ea6ca104b323e3
rick-sys <ricksanchezy2x@gmail.com>
2024-03-01 19:12:47 +0100

    dfdfd

--- components/component/main-hero.tsx
+++ components/component/main-hero.tsx
@@ -9,13 +9,13 @@ export function MainHero() {
     return (
       <section className="w-full">
         <div className="container px-4 md:px-6 flex flex-col items-start gap-2">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Bring to life your digital clone</h1>
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Meet your digital twin</h1>
           <p className="py-2 text-lg max-w-[700px] text-gray-500 dark:text-gray-400">
-          Create your digital counterpart, and take breathtaking pictures featuring you.
-          It's pure magic!
+          Create your AI counterpart, and take breathtaking pictures featuring you.
+          It's you, in your perfect form. It's pure magic! ✨
           </p>
           <div className="flex items-center">
-             <span className="text-sm font-thin">from </span><span className="text-lg font-bold">12€</span>
+             <span className="text-sm font-thin">24 masterpiece photo of you </span><span className="text-lg font-bold">12€ </span>
              <Link
               className="inline-flex w-100 items-center rounded-full shadow-lg hover:shadow-xl bg-pink-50 hover:bg-pink-100 dark:bg-pink-100 text-black-800 text-sm font-normal px-2 py-1 mr-2"
               href="./login"
@@ -34,27 +34,7 @@ export function MainHero() {
               }}
               width={34}
             />
-            I'am a girl&nbsp;&nbsp; 
-            </Link>
-            <Link
-              className="inline-flex w-100 items-center rounded-full shadow-lg hover:shadow-xl bg-blue-50 hover:bg-blue-100 dark:bg-blue-100 text-black-800 text-sm font-normal px-2 py-1 ml-2"
-              href="./login"
-              style={{
-                borderRadius: "50px",
-              }}
-            >
-            <img
-              alt="Double helix"
-              className="rounded-full mr-2"
-              height={34}
-              src="/kep2.gif"
-              style={{
-                aspectRatio: "34/34",
-                objectFit: "cover",
-              }}
-              width={34}
-            />
-            I'am a boy&nbsp;&nbsp; 
+            Create my clone&nbsp;&nbsp; 
             </Link>
           </div>
         </div>
