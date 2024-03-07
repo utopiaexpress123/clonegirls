7850b48603bc91d472fa9e23c5326c3b7b035bed
rick-sys <ricksanchezy2x@gmail.com>
2024-03-07 13:45:23 +0100

    dfd

--- app/overview/models/train/page.tsx
+++ app/overview/models/train/page.tsx
@@ -21,7 +21,7 @@ import {
 
 export default async function Index() {
   return (
-    <div className="w-full max-w-2xl mx-auto">
+    <div className="w-full bg-slate-100 p-4 rounded-xl shadow-xl">
       <div
         id="train-model-container"
         className="flex flex-1 flex-col gap-2 px-2"
