690f95a83649ac5137915fc794de2c66f5419822
rick-sys <ricksanchezy2x@gmail.com>
2024-03-11 20:19:41 +0100

    sdsds

--- components/TrainModelZone.tsx
+++ components/TrainModelZone.tsx
@@ -53,6 +53,17 @@ export default function TrainModelZone() {
           (file: File) => !files.some((f) => f.name === file.name)
         ) || [];
 
+      // if user tries to upload more than 10 files, display a toast
+//      if (newFiles.length + files.length > 10) {
+//        toast({
+//          title: "Too many images",
+//          description:
+//            "You can only upload up to 10 images in total. Please try again.",
+//          duration: 5000,
+//        });
+//        return;
+//      }
+
       // display a toast if any duplicate files were found
       if (newFiles.length !== acceptedFiles.length) {
         toast({
@@ -263,7 +274,8 @@ export default function TrainModelZone() {
           >
             <FormLabel>Samples</FormLabel>
             <FormDescription>
-              Upload images of the person you want to generate headshots for.
+              Upload 4-10 images of the person you want to generate headshots
+              for.
             </FormDescription>
             <div className="outline-dashed outline-2 outline-gray-100 hover:outline-blue-500 w-full h-full rounded-md p-4 flex justify-center align-middle">
               <input {...getInputProps()} />
